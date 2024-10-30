a3e07156d064b9ca7aaec53b61792c370460fc19
rosu <mentitas@tuta.io>
2024-10-30 16:34:00 -0300

    Habia que rebuildear lpmm

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -18,6 +18,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/restaurant', function () {
+    return Inertia::render('Restaurant');
+})->middleware(['auth', 'verified'])->name('restaurant');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
