7d260870a3b5789f05d0cfe46eab7fe78e1538db
rosu <mentitas@tuta.io>
2024-11-07 15:12:38 -0300

    arreglé sign up!

--- laravel-project/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravel-project/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('restaurant'); // Todo: que linkee a un restaurant de la tabla 'restaurant'
+            $table->string('restaurant')->nullable(); // Todo: que linkee a un restaurant de la tabla 'restaurant'
             $table->rememberToken();
             $table->timestamps();
         });
