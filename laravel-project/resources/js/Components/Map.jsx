23125af44edf37b54117e8b51ace504852e08b1b
jugoprex <jgoria@dc.uba.ar>
2024-11-07 20:11:16 -0300

    deshago cambios al mapa simple

--- laravel-project/resources/js/Components/Map.jsx
+++ laravel-project/resources/js/Components/Map.jsx
@@ -1,33 +1,22 @@
-import { useEffect, useState, useRef } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useRef } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const SimpleMap = () => {
-  const mapRef = useRef(null);
-  const [locations, setLocations] = useState([]);
-  const latitude = 51.505;
-  const longitude = -0.09;
-
-  useEffect(() => {
-    // Fetch restaurant locations from the backend
-    fetch('/api/restaurants/locations')
-      .then((response) => response.json())
-      .then((data) => setLocations(data))
-      .catch((error) => console.error('Error fetching locations:', error));
-  }, []);
-
-  return (
-    <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{ height: '100vh', width: '100vw' }}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {locations.map((location, index) => (
-        <Marker key={index} position={[location.latitude, location.longitude]}>
-          <Popup>{location.name}</Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-  );
-};
-
-export default SimpleMap;
+    const mapRef = useRef(null);
+    const latitude = 51.505;
+    const longitude = -0.09;
+  
+    return ( 
+      // Make sure you set the height and width of the map container otherwise the map won't show
+        <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{height: "100vh", width: "100vw"}}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {/* Additional map layers or components can be added here */}
+        </MapContainer>
+    );
+  };
+  
+  export default SimpleMap;
