20fce125e742923847b34d81fb24e62ab32c9dcf
jugoprex <jgoria@dc.uba.ar>
2024-11-07 18:33:03 -0300

    agrego ruta /map con un mapa basico

---
+++ laravel-project/resources/js/Pages/Map.jsx
@@ -0,0 +1,9 @@
+import SimpleMap from '@/Components/Map.jsx';
+
+export default function map() {
+  return (
+    <div>
+      <h1>My Leaflet.js and React Map</h1>
+      <SimpleMap />
+    </div>);
+}
--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -14,6 +14,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/map', function () {
+    return Inertia::render('Map');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
