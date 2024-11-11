b2ce2ff90b119c26ed2c8a7d89bad8b91c7e1034
jugoprex <jgoria@dc.uba.ar>
2024-11-10 21:48:32 -0300

    rename al layout que tiene el prompt para el login en la esquina

--- laravel-project/resources/js/Layouts/LayoutWrapper.jsx
+++ laravel-project/resources/js/Layouts/LayoutWrapper.jsx
@@ -1,11 +1,11 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import DefaultLayout from '@/Layouts/DefaultLayout';
+import LoginPromptLayout from '@/Layouts/LoginPromptLayout';
 import { usePage } from '@inertiajs/react';
 
 export default function LayoutWrapper({ children }) {
     const { auth } = usePage().props;
 
-    const Layout = auth?.user ? AuthenticatedLayout : DefaultLayout;
+    const Layout = auth?.user ? AuthenticatedLayout : LoginPromptLayout;
 
     return <Layout>{children}</Layout>;
 }
