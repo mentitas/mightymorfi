7abd64468057556354d2666bd016bdfa2619f8dc
jugoprex <jgoria@dc.uba.ar>
2024-11-10 21:29:39 -0300

    ahora el mapa es la raiz del sitio

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -7,15 +7,6 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/map', function () {
     return Inertia::render('Map');
 });
 
